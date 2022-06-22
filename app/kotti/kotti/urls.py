71967609b252b364ad7df37f87e20cf89db60c8d
rikeardi <risto.lievonen@gmail.com>
2022-06-22 22:39:59 +0300

    ok

--- app/kotti/kotti/urls.py
+++ app/kotti/kotti/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from ..main import api
+from main import api
 
 router = routers.DefaultRouter()
 router.register(r'rooms', api.RoomViewSet)
