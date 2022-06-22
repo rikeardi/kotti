5e4236d2d518fc7b0c5add04b567a2523798b6b1
rikeardi <risto.lievonen@gmail.com>
2022-06-22 18:53:44 +0300

    ok

--- app/kotti/main/admin.py
+++ app/kotti/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from kotti.models import *
+
+admin.site.register(KottiUser)
+admin.site.register(OpenTime)
+admin.site.register(Room)
+admin.site.register(Table)
+admin.site.register(TableReservation)
