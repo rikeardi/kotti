87a79ec6330e7d8e801291803e3e114b39d8b64a
rikeardi <risto.lievonen@gmail.com>
2022-06-22 20:15:23 +0300

    ok

--- app/kotti/main/urls.py
+++ app/kotti/main/urls.py
@@ -9,6 +9,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/login/', django.contrib.auth.views.LoginView.as_view(authentication_form=forms.LoginForm), name='login'),
-    path('accounts/create/', views.register, name='register'),
+    path('accounts/create/', django.contrib.auth.views.LoginView.as_view(authentication_form=forms.RegisterForm), name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
