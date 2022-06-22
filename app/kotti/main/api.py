8d64b13012e21cfef8d3f2b573155802ee86c643
rikeardi <risto.lievonen@gmail.com>
2022-06-22 22:43:47 +0300

    ok

--- app/kotti/main/api.py
+++ app/kotti/main/api.py
@@ -11,3 +11,47 @@ class RoomSerializer(serializers.HyperlinkedModelSerializer):
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class TableSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Table
+        fields = ('id', 'room', 'name', 'capacity')
+
+
+class TableViewSet(viewsets.ModelViewSet):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+
+
+class TableReservationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TableReservation
+        fields = ('id', 'table', 'user', 'start_time', 'end_time')
+
+
+class TableReservationViewSet(viewsets.ModelViewSet):
+    queryset = TableReservation.objects.all()
+    serializer_class = TableReservationSerializer
+
+
+class OpenTimeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OpenTime
+        fields = ('id', 'start_time', 'end_time')
+
+
+class OpenTimeViewSet(viewsets.ModelViewSet):
+    queryset = OpenTime.objects.all()
+    serializer_class = OpenTimeSerializer
+
+
+class KottiUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = KottiUser
+        fields = ('id', 'username', 'is_active', 'is_admin')
+
+
+class KottiUserViewSet(viewsets.ModelViewSet):
+    queryset = KottiUser.objects.all()
+    serializer_class = KottiUserSerializer
