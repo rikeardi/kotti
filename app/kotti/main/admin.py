6133b2c8622c88d6d589ae99ed9fe59441cb6dc2
rikeardi <risto.lievonen@gmail.com>
2022-06-22 21:28:44 +0300

    ok

--- app/kotti/main/admin.py
+++ app/kotti/main/admin.py
@@ -1,24 +1,54 @@
 from django.contrib import admin
 
 # Register your models here.
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from main.models import *
 
 
-class KottiUserAdmin(admin.StackedInline):
-    model = KottiUser
-    can_delete = False
-    verbose_name_plural = 'KottiUsers'
+class KottiUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = KottiUser
+        fields = ('email', 'first_name', 'last_name', 'phone', 'department', 'team')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+
+        if commit:
+            user.save()
+        return user
 
 
-class UserAdmin(UserAdmin):
-    inlines = (KottiUserAdmin,)
+class KottiUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = KottiUser
+        fields = ('email', 'first_name', 'last_name', 'phone', 'department', 'team', 'password', 'is_active', 'is_admin')
 
 
-#admin.site.unregister(User)
-admin.site.register(KottiUser, UserAdmin)
+class KottiUserAdmin(UserAdmin):
+    form = KottiUserChangeForm
+    add_form = KottiUserCreationForm
+
+
+
+admin.site.register(KottiUser, KottiUserAdmin)
+admin.site.unregister(Group)
 admin.site.register(OpenTime)
 admin.site.register(Room)
 admin.site.register(Table)
