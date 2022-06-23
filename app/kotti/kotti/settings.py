7635bed33b421f6cf133d4f4359063ccf2d37013
rikeardi <risto.lievonen@gmail.com>
2022-06-23 17:30:45 +0300

    ok

--- app/kotti/kotti/settings.py
+++ app/kotti/kotti/settings.py
@@ -110,7 +110,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 # Password validation
