23f3a569e57715ba9af6049b167596c2dd73bf8b
rikeardi <risto.lievonen@gmail.com>
2022-06-28 10:33:57 +0300

    ok

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,7 @@
 
 cd /code
 
-python manage.py makemigrations
+python manage.py makemigrations --noinput
 python manage.py migrate
 python manage.py createsuperuser --noinput
 python manage.py runserver 0.0.0.0:8000
