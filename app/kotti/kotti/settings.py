d1319055fa46de1e9f1939ffa7bdc754f504c3fb
rikeardi <risto.lievonen@gmail.com>
2022-06-30 19:10:44 +0300

    ok

--- app/kotti/kotti/settings.py
+++ app/kotti/kotti/settings.py
@@ -158,6 +158,8 @@ STATICFILES_DIRS = [
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/code/kotti/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
