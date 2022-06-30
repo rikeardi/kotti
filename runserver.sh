0c4df5f841b93a6c18294b8cc11f520e17f5fec2
rikeardi <risto.lievonen@gmail.com>
2022-06-30 20:05:56 +0300

    ok

--- runserver.sh
+++ runserver.sh
@@ -6,4 +6,5 @@ python manage.py makemigrations --noinput
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
-python manage.py runserver 0.0.0.0:8000
+
+gunicorn kotti.wsgi:application --bind 0.0.0.0:8000 --workers 4
