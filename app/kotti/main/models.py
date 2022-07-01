96014ff1a21fdb236f1bebc9d0528fd48285c33c
rikeardi <risto.lievonen@gmail.com>
2022-07-01 14:23:25 +0300

    ok

--- app/kotti/main/models.py
+++ app/kotti/main/models.py
@@ -131,7 +131,7 @@ class Booking(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.user.team} - {self.date.name} - {self.start_time} - {self.end_time}'
+        return f'{self.room__set.name} {self.user.team} - {self.date.name} - {self.start_time} - {self.end_time}'
 
 
 class Room(models.Model):
