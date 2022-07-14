9cffec379d70bd3e78288e5812f757b33da8fe1c
rikeardi <risto.lievonen@gmail.com>
2022-07-14 10:03:24 +0300

    ok

--- app/kotti/main/models.py
+++ app/kotti/main/models.py
@@ -156,8 +156,9 @@ class Room(models.Model):
         availability = []
 
         for open_time in self.open_times.all():
-            print(open_time.day.date)
-            print(date.today())
+            if(open_time.day.date < date.today()):
+                print (open_time.day.date)
+                print (date.today())
             day_availability = {'day': open_time.day.id, 'times': [], 'total_capacity': 0, 'total_bookings': 0, 'booking_percentage': 0 }
             for opening_time in open_time.times.all():
                 avail_time = opening_time.start_time
