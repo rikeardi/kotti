3e1dfa637f376c539a1a01313b62198db5b74c42
rikeardi <risto.lievonen@gmail.com>
2022-07-21 19:35:24 +0300

    ok

--- app/kotti/docs/urls.py
+++ app/kotti/docs/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('<page_name>/', views.page, name='page'),
     path('new-header/', views.new_header, name='new_header'),
     path('new-page/', views.new_page, name='new_page'),
+    path('<page_name>/', views.page, name='page'),
     path('', views.home, name='home'),
 ]
