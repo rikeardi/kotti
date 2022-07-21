1e71acb75e318a64c773e0ace7fcf402cc27c859
rikeardi <risto.lievonen@gmail.com>
2022-07-21 14:24:50 +0300

    ok

--- app/kotti/kotti/settings.py
+++ app/kotti/kotti/settings.py
@@ -150,7 +150,8 @@ STATIC_URL = '/static/'
 
 if DEBUG:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'docs/_build/html/static'),
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
