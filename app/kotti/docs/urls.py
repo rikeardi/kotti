8e65bc9d1fd6733566488c3f89d07c6813a915cb
rikeardi <risto.lievonen@gmail.com>
2022-07-21 20:45:52 +0300

    ok

--- app/kotti/docs/urls.py
+++ app/kotti/docs/urls.py
@@ -3,10 +3,18 @@ from . import views
 
 
 urlpatterns = [
-    path('new-header/', views.new_header, name='new_header'),
-    path('new-page/', views.new_page, name='new_page'),
+    path('<page_name>/', views.page, name='page'),
+    path('headers/<id>/delete/', views.header_delete, name='header_delete'),
+    path('headers/<id>/', views.header_edit, name='header_edit'),
+    path('headers/', views.header_new, name='header_new'),
     path('pages/<id>/delete/', views.page_delete, name='page_delete'),
     path('pages/<id>/', views.page_edit, name='page_edit'),
-    path('<page_name>/', views.page, name='page'),
+    path('pages/', views.page_new, name='page_new'),
+    path('chapters/<id>/delete/', views.chapter_delete, name='chapter_delete'),
+    path('chapters/<id>/', views.chapter_edit, name='chapter_edit'),
+    path('chapters/', views.chapter_new, name='chapter_new'),
+    path('sections/<id>/delete/', views.section_delete, name='section_delete'),
+    path('sections/<id>/', views.section_edit, name='section_edit'),
+    path('sections/', views.section_new, name='section_new'),
     path('', views.home, name='home'),
 ]
--- app/kotti/docs/views.py
+++ app/kotti/docs/views.py
@@ -13,6 +13,30 @@ def home(request):
     return render(request, 'docs/front.html', context)
 
 
+def header_delete(request, id):
+    header = DocsHeader.objects.get(id=id)
+    header.delete()
+    return redirect('/docs/')
+
+
+def header_edit(request, id):
+    header = DocsHeader.objects.get(id=id)
+
+    if request.method == 'POST':
+        header.title = request.POST['title'].lower()
+        header.save()
+        return redirect('/docs/')
+
+    return redirect('/docs/')
+
+
+def header_new(request):
+    if request.method == 'POST':
+        header = DocsHeader.objects.create(title=request.POST['title'].lower())
+        header.save()
+        return redirect('/docs/')
+
+
 def page(request, page_name):
     page = DocsPage.objects.get(title=page_name)
     context = {
@@ -23,6 +47,12 @@ def page(request, page_name):
         'page_chapters': DocsChapter.objects.filter(page_id=page.id),
     }
     return render(request, 'docs/front.html', context)
+
+
+def page_delete(request, id):
+    page = DocsPage.objects.get(id=id)
+    page.delete()
+    return redirect('/docs/')
 
 
 def page_edit(request, id):
@@ -36,23 +66,64 @@ def page_edit(request, id):
     return redirect('/docs/' + page.title + '/')
 
 
-def page_delete(request, id):
-    page = DocsPage.objects.get(id=id)
-    page.delete()
-    return redirect('/docs/')
-
-
-def new_header(request):
+def page_new(request):
     if request.method == 'POST':
-        header = DocsHeader.objects.create(title=request.POST['title'].lower())
-        header.save()
-        return redirect('/docs/')
-
-
-def new_page(request):
-    if request.method == 'POST':
-        print(request.POST)
         page = DocsPage.objects.create(title=request.POST['title'].lower(),
                                        header_id=request.POST['header_id'])
         page.save()
         return redirect('/docs/' + page.title + '/')
+
+
+def chapter_delete(request, id):
+    chapter = DocsChapter.objects.get(id=id)
+    chapter.delete()
+    return redirect('/docs/' + chapter.page.title + '/')
+
+
+def chapter_edit(request, id):
+    chapter = DocsChapter.objects.get(id=id)
+
+    if request.method == 'POST':
+        chapter.title = request.POST['title'].lower()
+        chapter.save()
+        return redirect('/docs/' + chapter.page.title + '/')
+
+    return redirect('/docs/' + chapter.page.title + '/')
+
+
+def chapter_new(request):
+    if request.method == 'POST':
+        chapter = DocsChapter.objects.create(title=request.POST['title'].lower(),
+                                             page_id=request.POST['page_id'])
+        chapter.save()
+        chapter.page = DocsPage.objects.get(id=request.POST['page_id'])
+        return redirect('/docs/' + chapter.page.title + '/')
+
+
+def section_delete(request, id):
+    section = DocsSection.objects.get(id=id)
+    section.delete()
+    return redirect('/docs/')
+
+
+def section_edit(request, id):
+    section = DocsSection.objects.get(id=id)
+
+    if request.method == 'POST':
+        if request.POST['text']:
+            section.text = request.POST['text']
+        if request.POST['image']:
+            print(request.POST['image'])
+        section.save()
+        return redirect('/docs/')
+
+    return redirect('/docs/')
+
+
+def section_new(request):
+    if request.method == 'POST':
+        section = DocsSection.objects.create(text=request.POST['text'].lower(),
+                                             order=request.POST['order'],
+                                             chapter_id=request.POST['chapter_id'])
+        section.save()
+        return redirect('/docs/')
